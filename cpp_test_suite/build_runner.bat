147204b3570fa80da64aaf91ab6554f3f4024423
trogucki <trogucki@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2011-07-08 09:23:08 +0000

    Updated build_runner.bat with new test suites

--- cpp_test_suite/build_runner.bat
+++ cpp_test_suite/build_runner.bat
@@ -1,1 +1,14 @@
-cxxtest\bin\cxxtestgen.py --template=cxxtest\template\tango_template.tpl -o runner.cpp test_suite\cxx_attr.cpp
+cxxtest\bin\cxxtestgen.py --template=cxxtest\template\tango_template.tpl -o runner.cpp ^
+cxx_blackbox.cpp ^
+cxx_misc.cpp ^
+cxx_attr_write.cpp ^
+cxx_attr_conf.cpp ^
+cxx_attr_misc.cpp ^
+cxx_attr.cpp ^
+cxx_misc_util.cpp ^
+cxx_templ_cmd.cpp ^
+cxx_exception.cpp ^
+cxx_cmd_query.cpp ^
+cxx_seq_vec.cpp ^
+cxx_test_state_on.cpp ^
+cxx_cmd_types.cpp
