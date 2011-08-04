2905cbcb62ad3def0200e026198262dc4632b2d0
trogucki <trogucki@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2011-08-04 13:33:17 +0000

    Added cxx_signal.cpp to the test suite under Windows

--- cpp_test_suite/build_runner.bat
+++ cpp_test_suite/build_runner.bat
@@ -1,6 +1,6 @@
 set REL_DIR=..\..\..\..\
 %REL_DIR%cxxtest\bin\cxxtestgen.py --template=%REL_DIR%cxxtest\template\tango_template.tpl -o runner.cpp ^
-%REL_DIR%test_suite\cxx_dserver_cmd.cpp ^
+%REL_DIR%test_suite\cxx_signal.cpp ^
 %REL_DIR%test_suite\cxx_dserver_misc.cpp ^
 %REL_DIR%test_suite\cxx_always_hook.cpp ^
 %REL_DIR%test_suite\cxx_blackbox.cpp ^
@@ -16,3 +16,5 @@ set REL_DIR=..\..\..\..\
 %REL_DIR%test_suite\cxx_test_state_on.cpp ^
 %REL_DIR%test_suite\cxx_misc.cpp ^
 %REL_DIR%test_suite\cxx_cmd_types.cpp
+
+rem %REL_DIR%test_suite\cxx_dserver_cmd.cpp
