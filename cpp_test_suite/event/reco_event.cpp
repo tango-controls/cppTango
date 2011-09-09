8b8535ebf2bd5d3be259a4e9180d609b03acd1cf
taurel <taurel@users.noreply.github.com>
2011-09-09 11:31:12 +0000

    Some code formatting

--- cpp_test_suite/event/reco_event.cpp
+++ cpp_test_suite/event/reco_event.cpp
@@ -107,11 +107,11 @@ int main(int argc, char **argv)
 	{
 		device = new DeviceProxy(device_name);
 	}
-        catch (CORBA::Exception &e)
-        {
-              	Except::print_exception(e);
+	catch (CORBA::Exception &e)
+	{
+		Except::print_exception(e);
 		exit(1);
-        }
+	}
 
 	coutv << endl << "new DeviceProxy(" << device->name() << ") returned" << endl << endl;
 
