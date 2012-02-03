56b8ef49b694e5f67359be599ff431f7d7aaa95d
taurel <taurel@users.noreply.github.com>
2012-02-03 12:42:11 +0000

    - Back to normal

--- cpp_test_suite/event/state_event.cpp
+++ cpp_test_suite/event/state_event.cpp
@@ -120,10 +120,6 @@ int main(int argc, char **argv)
 	}
 
 	coutv << endl << "new DeviceProxy(" << device->name() << ") returned" << endl << endl;
-
-int ctr = 0;
-while(1)
-{
 	
 	try
 	{
@@ -293,10 +289,6 @@ while(1)
 		Except::print_exception(ex);
 		exit(-1);
 	}
-
-ctr++;
-cout << "Loop nb " << ctr << endl;
-}
 
 	delete device;
 	
