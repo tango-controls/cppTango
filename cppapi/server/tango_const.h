224ffc1198167279217dce4d292eac4119af2a92
taurel <taurel@users.noreply.github.com>
2016-04-27 13:54:19 +0000

    - Bump release number to 9.2.4

--- cppapi/server/tango_const.h
+++ cppapi/server/tango_const.h
@@ -42,7 +42,7 @@ namespace Tango
 
 #define   TANGO_VERSION_MAJOR        9
 #define   TANGO_VERSION_MINOR        2
-#define   TANGO_VERSION_PATCH        3
+#define   TANGO_VERSION_PATCH        4
 
 #define   TANGO_BASE_CLASS           Tango::Device_5Impl
 
