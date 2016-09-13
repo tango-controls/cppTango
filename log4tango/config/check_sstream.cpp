beb2c8e9c8948b6b498ee4700fc2b9522f689182
ingvord <tla-ingvord@yandex.ru>
2016-09-13 21:30:56 +0300

    TANGO-678 progress (migrate to cmake)

---
+++ log4tango/config/check_sstream.cpp
@@ -0,0 +1,16 @@
+//
+// Created by ingvord on 9/13/16.
+//
+#include<iostream>
+
+int main(int argc,char **arv)
+{
+    std::ostringstream oss;
+
+    oss << "Hello";
+
+    std::cout << oss << std::endl;
+
+    return 0;
+}
+
---
+++ log4tango/config/config.cmake
@@ -0,0 +1,51 @@
+include(CheckIncludeFile)
+include(CheckFunctionExists)
+include(FindThreads)
+include(CheckCXXSourceCompiles)
+include(CheckTypeSize)
+
+macro (LOG4TANGO_CHECK_INCLUDE_FILE header var)
+    CHECK_INCLUDE_FILE("${header}"        LOG4TANGO_${var} )
+endmacro()
+
+LOG4TANGO_CHECK_INCLUDE_FILE("dlfcn.h"        HAVE_DLFCN_H)
+LOG4TANGO_CHECK_INCLUDE_FILE("inttypes.h"     HAVE_INTTYPES_H)
+LOG4TANGO_CHECK_INCLUDE_FILE("io.h"           HAVE_IO_H)
+LOG4TANGO_CHECK_INCLUDE_FILE("memory.h"       HAVE_MEMORY_H)
+LOG4TANGO_CHECK_INCLUDE_FILE("stdint.h"       HAVE_STDINT_H)
+LOG4TANGO_CHECK_INCLUDE_FILE("stdlib.h"       HAVE_STDLIB_H)
+LOG4TANGO_CHECK_INCLUDE_FILE("strings.h"      HAVE_STRINGS_H)
+LOG4TANGO_CHECK_INCLUDE_FILE("string.h"       HAVE_STRING_H)
+LOG4TANGO_CHECK_INCLUDE_FILE("sys/stat.h"     HAVE_SYS_STAT_H)
+LOG4TANGO_CHECK_INCLUDE_FILE("sys/types.h"    HAVE_SYS_TYPES_H)
+LOG4TANGO_CHECK_INCLUDE_FILE("unistd.h"       HAVE_UNISTD_H)
+
+macro (LOG4TANGO_CHECK_FUNCTION_EXISTS function var)
+    CHECK_FUNCTION_EXISTS("${function}"        LOG4TANGO_${var} )
+endmacro()
+#check function
+LOG4TANGO_CHECK_FUNCTION_EXISTS(ftime         HAVE_FTIME)
+LOG4TANGO_CHECK_FUNCTION_EXISTS(gettimeofday  HAVE_GETTIMEOFDAY)
+
+
+#check threads
+check_library_exists(pthread pthread_attr_get_np "" HAVE_PTHREAD_ATTR_GET_NP)
+check_library_exists(pthread pthread_getattr_np "" HAVE_PTHREAD_GETATTR_NP)
+check_library_exists(pthread pthread_create "" HAVE_PTHREAD_CREATE)
+check_include_file("pthread.h;pthread_np.h" HAVE_PTHREAD_NP_H)
+
+#check compiler features
+file(READ "config/check_sstream.cpp" SSTREAM_CHECK_SOURCE)
+check_cxx_source_compiles("${SSTREAM_CHECK_SOURCE}"  SSTREAM_CHECK)
+if(NOT BIND_COMPILER_CHECK)
+    set(LOG4TANGO_HAVE_SSTREAM "/**/")
+endif()
+
+#check types
+check_type_size(int64_t INT64_SIZE)
+if(${INT64_SIZE} EQUAL 8)
+    set(LOG4TANGO_HAVE_INT64_T "/**/")
+endif()
+
+
+configure_file(config/config.h.in ${PROJECT_BINARY_DIR}/log4tango/include/log4tango/config.h)
