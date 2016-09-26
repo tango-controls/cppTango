299414b2c2c6f0219a4dd033c522836c84e57b6c
ingvord <tla-ingvord@yandex.ru>
2016-09-26 23:16:16 +0300

    Progress #9

--- .travis/run.sh
+++ .travis/run.sh
@@ -9,21 +9,21 @@ docker exec cpp_tango make -C /src/idl/build install
 
 echo "Build cppTango"
 docker exec cpp_tango cmake -H/src -B/src/build -DCMAKE_VERBOSE_MAKEFILE=true
-RV=$?
-if [ RV -ne 0 ]; then
+if [ $? -ne 0 ]; then
+    RV=$?
     echo "FAILED! RV=$RV" >&2
     exit RV
 fi
 docker exec cpp_tango make -C /src/build
-RV=$?
-if [ RV -ne 0 ]; then
+if [ $? -ne 0 ]; then
+    RV=$?
     echo "FAILED! RV=$RV" >&2
     exit RV
 fi
 echo "Test cppTango"
 docker exec cpp_tango /bin/sh -c 'cd /src/build; exec ctest -V'
-RV=$?
-if [ RV -ne 0 ]; then
+if [ $? -ne 0 ]; then
+    RV=$?
     echo "FAILED! RV=$RV" >&2
     exit RV
 fi
