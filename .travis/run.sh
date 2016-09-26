af31ca810dc2f068d73ebf847594252e5743caec
ingvord <tla-ingvord@yandex.ru>
2016-09-26 21:12:06 +0300

    Progress #9

--- .travis/run.sh
+++ .travis/run.sh
@@ -8,4 +8,12 @@ echo "Install tango-idl"
 docker exec cpp_tango make -C /src/idl/build install
 
 echo "Build cppTango"
-docker exec cpp_tango cmake -H/src -B/src/build -DCMAKE_VERBOSE_MAKEFILE=true --build /src/build -- test
+docker exec cpp_tango cmake -H/src -B/src/build -DCMAKE_VERBOSE_MAKEFILE=true
+if [ $? -ne 0 ]; then
+    echo "FAILED!" >&2
+fi
+
+docker exec cpp_tango cmake --build /src/build -- test
+if [ $? -ne 0 ]; then
+    echo "FAILED!" >&2
+fi
