f29a489b5e7447216dcad27685c2ba60119f7370
ingvord <tla-ingvord@yandex.ru>
2016-09-27 12:31:31 +0300

    Progress #9

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-echo $CMAKE_BUILD_TYPE
-
 docker exec cpp_tango mkdir -p /src/idl/build
 docker exec cpp_tango mkdir -p /src/build
 
@@ -8,8 +6,8 @@ echo "Build tango-idl"
 docker exec cpp_tango cmake -H/src/idl -B/src/idl/build
 echo "Install tango-idl"
 docker exec cpp_tango make -C /src/idl/build install
-echo "Build cppTango"
-docker exec cpp_tango cmake -H/src -B/src/build -DCMAKE_VERBOSE_MAKEFILE=true
+echo "Build cppTango:$CMAKE_BUILD_TYPE"
+docker exec cpp_tango cmake -H/src -B/src/build -DCMAKE_VERBOSE_MAKEFILE=true -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE
 docker exec cpp_tango make -C /src/build -j 2
 echo "Test cppTango"
 docker exec cpp_tango /bin/sh -c 'cd /src/build; exec ctest -V'
