424ec03499d4a98e7cab50790ff0c23c309cd9f6
ingvord <tla-ingvord@yandex.ru>
2016-10-01 11:17:27 +0300

    Progress #268: fix travis build

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-docker exec cpp_tango mkdir -p /home/tango/src/idl/build
+docker exec cpp_tango mkdir -p /home/tango/idl/build
 docker exec cpp_tango mkdir -p /home/tango/src/build
 
 echo "Build tango-idl"
-docker exec cpp_tango cmake -H/home/tango/src/idl -B/home/tango/src/idl/build -DCMAKE_INSTALL_PREFIX=/home/tango
+docker exec cpp_tango cmake -H/home/tango/idl -B/home/tango/idl/build -DCMAKE_INSTALL_PREFIX=/home/tango
 echo "Install tango-idl"
-docker exec cpp_tango make -C /home/tango/src/idl/build install
+docker exec cpp_tango make -C /home/tango/idl/build install
 echo "Build cppTango:$CMAKE_BUILD_TYPE"
 docker exec cpp_tango cmake -H/home/tango/src -B/home/tango/src/build -DCMAKE_VERBOSE_MAKEFILE=true -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE
 docker exec cpp_tango make -C /home/tango/src/build -j 2
