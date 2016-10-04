973ea35357a18fcccee0780f0e59bb6f450bc6a9
ingvord <tla-ingvord@yandex.ru>
2016-10-04 16:11:45 +0300

    Progress #268: check cmake return value

--- .travis/run.sh
+++ .travis/run.sh
@@ -4,7 +4,7 @@ docker exec cpp_tango mkdir -p /home/tango/src/build
 
 echo "Build tango-idl"
 docker exec cpp_tango cmake -H/home/tango/idl -B/home/tango/idl/build -DCMAKE_INSTALL_PREFIX=/home/tango
-if [ $1 -ne "0" ]
+if [ $! -ne "0" ]
 then
 	echo "CMake returns non zero !!!!!"
 	date
@@ -14,7 +14,7 @@ echo "Install tango-idl"
 docker exec cpp_tango make -C /home/tango/idl/build install
 echo "Build cppTango:$CMAKE_BUILD_TYPE"
 docker exec cpp_tango cmake -H/home/tango/src -B/home/tango/src/build -DCMAKE_VERBOSE_MAKEFILE=true -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE
-if [ $1 -ne "0" ]
+if [ $! -ne "0" ]
 then
 	echo "CMake returns non zero !!!!!"
 	date
