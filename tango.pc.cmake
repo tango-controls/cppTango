506f88f0da99c818b6d340e020f79daefd6e1eeb
ingvord <tla-ingvord@yandex.ru>
2016-10-07 16:57:09 +0300

    adjust pkg-config file

--- tango.pc.cmake
+++ tango.pc.cmake
@@ -7,5 +7,5 @@ Name: @CMAKE_PROJECT_NAME@
 Description: TANGO client/server API library
 Version: @LIBRARY_VERSION@
 Cflags: -std=c++11 -I${includedir}
-Requires: omniDynamic4 >= 4.1.6, omniCOS4, log4tango, libzmq
+Requires: omniDynamic4 >= 4.1.6, omniCOS4, libzmq
 Libs: -L${libdir} -ltango
