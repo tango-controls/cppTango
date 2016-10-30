054dee0b119937e5775ea459db4f215a6740d9bd
ingvord <tla-ingvord@yandex.ru>
2016-10-30 10:57:34 +0300

    Progress: fix tests, cxx_always_hook

--- test/cpp_test_suite/new_tests/cxx_always_hook.cpp
+++ test/cpp_test_suite/new_tests/cxx_always_hook.cpp
@@ -218,6 +218,7 @@ public:
 			v_s.push_back("Event heartbeat");
 			v_s.push_back("Sub device property");
 			v_s.push_back("Sleep for :");
+			v_s.push_back("client_ip=");
 			CmpTst::CompareTest::out_remove_entries(out_file,v_s);
 
 			map<string,string> prop_val_map;
