3846d3dc090ce01bc2323493d3290a5917d96149
ingvord <tla-ingvord@yandex.ru>
2017-06-01 11:11:14 +0300

    @taurel code review progress: encapsulate *_enum_labels

--- test/cpp_test_suite/cpp_test_ds/TypeCmds.cpp
+++ test/cpp_test_suite/cpp_test_ds/TypeCmds.cpp
@@ -1631,8 +1631,8 @@ CORBA::Any *OEncoded::execute(Tango::DeviceImpl *device,const CORBA::Any &in_any
 
 IODevEnum::IODevEnum() :
         Command("IODevEnum", Tango::CmdArgType::DEV_ENUM, Tango::CmdArgType::DEV_ENUM, "DevEnum", "DevEnum"){
-    get_ext()->in_enum_labels = {"IN Label 1", "IN Label 2", "IN Label 3"};
-    get_ext()->out_enum_labels = {"OUT Label 1", "OUT Label 2", "OUT Label 3"};
+    set_in_enum_labels({"IN Label 1", "IN Label 2", "IN Label 3"});
+    set_out_enum_labels({"OUT Label 1", "OUT Label 2", "OUT Label 3"});
 }
 
 bool IODevEnum::is_allowed(Tango::DeviceImpl *, const CORBA::Any &) {
