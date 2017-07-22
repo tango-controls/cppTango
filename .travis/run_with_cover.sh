d510b59551dcbd60edce415a43cffe01f81481a3
Ingvord <tla-ingvord@yandex.ru>
2017-07-22 14:18:07 +0300

    Integrate with Coveralls (#382)
    Close #369

--- .travis/run_with_cover.sh
+++ .travis/run_with_cover.sh
@@ -5,11 +5,13 @@ echo "TANGO_HOST=$TANGO_HOST"
 docker exec cpp_tango mkdir -p /home/tango/src/build
 
 echo "Run cmake cppTango in $CMAKE_BUILD_TYPE mode"
-docker exec cpp_tango cmake -H/home/tango/src -B/home/tango/src/build -DCMAKE_VERBOSE_MAKEFILE=true -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE
+echo "Using COVERALLS=$COVERALLS"
+docker exec cpp_tango cmake -H/home/tango/src -B/home/tango/src/build -DCOVERALLS=$COVERALLS -DCOVERALLS_MODULE_PATH=/home/tango/coveralls-cmake/cmake -DCMAKE_VERBOSE_MAKEFILE=true -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE
 if [ $? -ne "0" ]
 then
     exit -1
 fi
+echo "Run make cppTango"
 docker exec cpp_tango make -C /home/tango/src/build -j 2
 if [ $? -ne "0" ]
 then
@@ -22,7 +24,7 @@ then
     exit -1
 fi
 echo "Run tests"
-docker exec cpp_tango /bin/sh -c 'cd /home/tango/src/build; exec ctest'
+docker exec cpp_tango /bin/sh -c 'cd /home/tango/src/build; exec make coveralls'
 if [ $? -ne "0" ]
 then
     exit -1
