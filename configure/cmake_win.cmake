f5c61e9321f895952f243d61f0d3325104b3a6ba
NexeyaSGara <sebastien.gara@nexeya.com>
2017-08-20 01:26:19 +0200

    Update cmake_win.cmake

--- configure/cmake_win.cmake
+++ configure/cmake_win.cmake
@@ -40,8 +40,9 @@ add_library(tangod SHARED    $<TARGET_OBJECTS:log4tango_objects>
                             $<TARGET_OBJECTS:jpeg_mmx_objects>
                             $<TARGET_OBJECTS:server_objects>)
                             
-SET_TARGET_PROPERTIES(tangod-static PROPERTIES OUTPUT_NAME "tangod")
-SET_TARGET_PROPERTIES(tangod-static PROPERTIES PREFIX "lib")
+SET_TARGET_PROPERTIES(tangod-static PROPERTIES OUTPUT_NAME "tangod" CLEAN_DIRECT_OUTPUT 1)
+SET_TARGET_PROPERTIES(tangod PROPERTIES OUTPUT_NAME "tangod" CLEAN_DIRECT_OUTPUT 1)
+#SET_TARGET_PROPERTIES(tangod-static PROPERTIES PREFIX "lib")
 
 target_link_libraries(tangod PUBLIC ${WIN32_LIBS} ${OMNIORB_PKG_LIBRARIES} ${ZMQ_PKG_LIBRARIES} ${PTHREAD_WIN32_LIBS} ${CMAKE_DL_LIBS})
 target_link_libraries(tangod-static PUBLIC ${WIN32_LIBS} ${OMNIORB_PKG_LIBRARIES} ${ZMQ_PKG_LIBRARIES} ${PTHREAD_WIN32_LIBS} ${CMAKE_DL_LIBS})
@@ -85,8 +86,9 @@ add_library(tango SHARED    $<TARGET_OBJECTS:log4tango_objects>
                             $<TARGET_OBJECTS:jpeg_objects>
                             $<TARGET_OBJECTS:jpeg_mmx_objects>
                             $<TARGET_OBJECTS:server_objects>)
-SET_TARGET_PROPERTIES(tango-static PROPERTIES OUTPUT_NAME "tango")
-SET_TARGET_PROPERTIES(tango-static PROPERTIES PREFIX "lib")
+SET_TARGET_PROPERTIES(tango-static PROPERTIES OUTPUT_NAME "tango" CLEAN_DIRECT_OUTPUT 1)
+SET_TARGET_PROPERTIES(tango PROPERTIES OUTPUT_NAME "tango" CLEAN_DIRECT_OUTPUT 1)
+#SET_TARGET_PROPERTIES(tango-static PROPERTIES PREFIX "lib")
                             
 target_link_libraries(tango PUBLIC ${WIN32_LIBS} ${OMNIORB_PKG_LIBRARIES} ${ZMQ_PKG_LIBRARIES} ${PTHREAD_WIN32_LIBS} ${CMAKE_DL_LIBS})
 target_link_libraries(tango-static PUBLIC ${WIN32_LIBS} ${OMNIORB_PKG_LIBRARIES} ${ZMQ_PKG_LIBRARIES} ${PTHREAD_WIN32_LIBS} ${CMAKE_DL_LIBS})
