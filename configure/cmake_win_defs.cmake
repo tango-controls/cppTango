39db9c5ebba62b3d63678d0b6526cb2aea9b9213
NexeyaSGara <sebastien.gara@nexeya.com>
2017-11-06 23:54:14 +0100

    Update cmake_win_defs.cmake

--- configure/cmake_win_defs.cmake
+++ configure/cmake_win_defs.cmake
@@ -1,10 +1,9 @@
 #definitions and preprocessor
-add_definitions(-D__x86__)
+add_definitions(-D_WIN32)
 add_definitions(-D__POSIX_NT__)
 add_definitions(-D__OSVERSION__=4)
 add_definitions(-DOMNI_UNLOADABLE_STUBS)
 add_definitions(-D_CRT_SECURE_NO_WARNINGS)
-add_definitions(-DWIN32)
 add_definitions(-DHAVE_STRUCT_TIMESPEC)
 add_definitions(-D_TIMESPEC_DEFINED)
 add_definitions(-DSTRUCT_TIMESPEC_HAS_TV_SEC)
