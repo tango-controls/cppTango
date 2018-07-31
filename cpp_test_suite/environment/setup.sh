fb70f20920230d5ee2bb2f364054b5bc8b122f1e
ingvord <mail@ingvord.ru>
2018-07-31 13:47:01 +0200

    integrate docker-compose into cmake [15m]

--- cpp_test_suite/environment/setup.sh
+++ cpp_test_suite/environment/setup.sh
@@ -2,36 +2,11 @@
 
 echo "Setup test environment"
 
-#run mysql-tango docker
-docker run --name mysql_db \
-    -e MYSQL_ROOT_PASSWORD=root \
-    -d tangocs/mysql:9.2.2 --sql-mode=""
+docker-compose -f $1 up start_dependencies
 
-#run tango-cs docker
-CONTAINER=$(docker run --name tango_cs \
-    -p 10000:10000 \
-    -e TANGO_HOST=127.0.0.1:10000 \
-    -e MYSQL_HOST=mysql_db:3306 \
-    -e MYSQL_USER=tango \
-    -e MYSQL_PASSWORD=tango \
-    -e MYSQL_DATABASE=tango \
-    --link mysql_db:mysql_db \
-    -d tangocs/tango-cs:latest)
+docker-compose -f $1 up -d tango-test
 
-echo "CONTAINER=$CONTAINER"
-
-IPADDR=$(docker inspect -f '{{ .NetworkSettings.IPAddress }}' $CONTAINER)
 #export TANGO_HOST
-TANGO_HOST=$IPADDR:10000
+TANGO_HOST=localhost:10000
 echo "TANGO_HOST=$TANGO_HOST"
 export TANGO_HOST
-
-echo "Create tango_host file"
-echo "#!/bin/bash" > tango_host
-echo "export TANGO_HOST=$TANGO_HOST" >> tango_host
-
-echo "Wait till tango-cs is online"
-#TODO notification?
-sleep 30
-
-#ctest
--- cpp_test_suite/environment/shutdown.sh
+++ cpp_test_suite/environment/shutdown.sh
@@ -2,15 +2,4 @@
 
 echo "Shutdown test environment"
 
-#kill tango-cs docker
-docker stop tango_cs
-docker rm tango_cs
-
-#kill mysql-tango docker
-docker stop mysql_db
-docker rm mysql_db
-
-echo "Revert tango_host file"
-echo "TANGO_HOST=$TANGO_HOST"
-echo "#!/bin/bash" > tango_host
-echo "export TANGO_HOST=$TANGO_HOST" >> tango_host
+docker-compose -f $1 stop
