c36351d56895d373aee7187755e4917949527121
ingvord <tla-ingvord@yandex.ru>
2018-08-22 16:53:06 +0300

    some refactoring [1h]

--- .travis/debian8/run.sh
+++ .travis/debian8/run.sh
@@ -7,41 +7,10 @@ docker exec cpp_tango mkdir -p /home/tango/src/build
 echo "Run cmake cppTango in $CMAKE_BUILD_TYPE mode"
 echo "Using COVERALLS=$COVERALLS"
 
-TEST_COMMAND="exec ctest"
-if [ $COVERALLS = "ON" ]
-then
-    TEST_COMMAND="exec make coveralls"
-fi
-
 docker exec cpp_tango cmake -H/home/tango/src -B/home/tango/src/build -DCOVERALLS=$COVERALLS -DCOVERALLS_MODULE_PATH=/home/tango/coveralls-cmake/cmake -DCMAKE_VERBOSE_MAKEFILE=true -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE
 if [ $? -ne "0" ]
 then
     exit -1
 fi
-docker exec cpp_tango make -C /home/tango/src/build -j 2
-if [ $? -ne "0" ]
-then
-    exit -1
-fi
 
 docker exec cpp_tango make -C /home/tango/src/build -j 2
-echo "PreTest"
-docker exec cpp_tango /bin/sh -c 'cd /home/tango/src/build/cpp_test_suite/environment; exec ./pre_test.sh'
-if [ $? -ne "0" ]
-then
-    exit -1
-fi
-echo "Test"
-echo "TEST_COMMAND=$TEST_COMMAND"
-docker exec cpp_tango /bin/sh -c "cd /home/tango/src/build; $TEST_COMMAND"
-if [ $? -ne "0" ]
-then
-    exit -1
-fi
-
-echo "PostTest"
-docker exec cpp_tango /bin/sh -c 'cd /home/tango/src/build/cpp_test_suite/environment; exec ./post_test.sh'
-if [ $? -ne "0" ]
-then
-    exit -1
-fi
--- .travis/debian9/run.sh
+++ .travis/debian9/run.sh
@@ -1,29 +1,5 @@
 #!/usr/bin/env bash
-docker exec cpp_tango mkdir -p /home/tango/idl/build
-docker exec cpp_tango mkdir -p /home/tango/cppzmq/build
 docker exec cpp_tango mkdir -p /home/tango/src/build
-
-echo "Build cppzmq"
-docker exec cpp_tango cmake -H/home/tango/cppzmq -B/home/tango/cppzmq/build -DCMAKE_INSTALL_PREFIX=/home/tango
-if [ $? -ne "0" ]
-then
-    exit -1
-fi
-echo "Install cppzmq"
-docker exec cpp_tango make -C /home/tango/cppzmq/build install
-if [ $? -ne "0" ]
-then
-    exit -1
-fi
-
-echo "Build tango-idl"
-docker exec cpp_tango cmake -H/home/tango/idl -B/home/tango/idl/build -DCMAKE_INSTALL_PREFIX=/home/tango
-if [ $? -ne "0" ]
-then
-    exit -1
-fi
-echo "Install tango-idl"
-docker exec cpp_tango make -C /home/tango/idl/build install
 
 echo "Build cppTango:$CMAKE_BUILD_TYPE"
 docker exec cpp_tango cmake -H/home/tango/src -B/home/tango/src/build -DCMAKE_VERBOSE_MAKEFILE=true -DCPPZMQ_BASE=/home/tango -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE
@@ -33,7 +9,3 @@ then
 fi
 
 docker exec cpp_tango make -C /home/tango/src/build -j 2
-echo "Test log4tango"
-docker exec cpp_tango /bin/sh -c 'cd /home/tango/src/build/log4tango; exec ctest -V'
-echo "Test cppTango"
-docker exec cpp_tango /bin/sh -c 'cd /home/tango/src/build/cpp_test_suite; exec ctest -V'
