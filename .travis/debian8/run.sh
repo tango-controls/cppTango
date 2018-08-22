9ed5ab26ad291959dc3b4d69d6ad9cc04924fe74
Ingvord <mail@ingvord.ru>
2018-08-22 16:28:46 +0300

    Update run.sh [0,5h]

--- .travis/debian8/run.sh
+++ .travis/debian8/run.sh
@@ -7,7 +7,7 @@ docker exec cpp_tango mkdir -p /home/tango/src/build
 echo "Run cmake cppTango in $CMAKE_BUILD_TYPE mode"
 echo "Using COVERALLS=$COVERALLS"
 
-TEST_COMMAND="exec ctest -V"
+TEST_COMMAND="exec ctest"
 if [ $COVERALLS = "ON" ]
 then
     TEST_COMMAND="exec make coveralls"
@@ -25,15 +25,22 @@ then
 fi
 
 docker exec cpp_tango make -C /home/tango/src/build -j 2
-echo "Test log4tango"
-docker exec cpp_tango /bin/sh -c 'cd /home/tango/src/build/log4tango; exec ctest -V'
+echo "PreTest"
+docker exec cpp_tango /bin/sh -c 'cd /home/tango/src/build/cpp_test_suite/environment; exec ./pre_test.sh'
 if [ $? -ne "0" ]
 then
     exit -1
 fi
-echo "Test cppTango"
+echo "Test"
 echo "TEST_COMMAND=$TEST_COMMAND"
-docker exec cpp_tango /bin/sh -c "cd /home/tango/src/build/cpp_test_suite; $TEST_COMMAND"
+docker exec cpp_tango /bin/sh -c "cd /home/tango/src/build; $TEST_COMMAND"
+if [ $? -ne "0" ]
+then
+    exit -1
+fi
+
+echo "PostTest"
+docker exec cpp_tango /bin/sh -c 'cd /home/tango/src/build/cpp_test_suite/environment; exec ./post_test.sh'
 if [ $? -ne "0" ]
 then
     exit -1
