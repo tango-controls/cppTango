62e9818e3b6926510e4b5a1ea4dbc822d7333b2a
Ingvord <tla-ingvord@yandex.ru>
2018-08-28 19:26:58 +0000

    Backport #369: Integrate with Coveralls (#473)
    * Backport #369: Integrate with Coveralls [25min]
    
    * fix build with coveralls [5min]
    
    * Update .travis.yml
    
    Fix coveralls build
    
    * Backport #369: Integrate with Coveralls; run coveralls [1h]
    
    * disable log4tango bench test
    
    * Update run.sh [0,5h]
    
    * some refactoring [1h]
    
    * Make .travis/**.sh executable
    
    * Update coveralls.cmake [5min]
    
    * Update README.md
    
    * add coverage report path [0,5h]
    
    * integrate sonar and coveralls [0,25h]

--- .travis/debian8/run.sh
+++ .travis/debian8/run.sh
@@ -1,25 +1,16 @@
 #!/usr/bin/env bash
-docker exec cpp_tango mkdir -p /home/tango/idl/build
+#this script is executed from within Travis environment. TANGO_HOST is already set
+echo "TANGO_HOST=$TANGO_HOST"
+
 docker exec cpp_tango mkdir -p /home/tango/src/build
 
-echo "Build tango-idl"
-docker exec cpp_tango cmake -H/home/tango/idl -B/home/tango/idl/build -DCMAKE_INSTALL_PREFIX=/home/tango
-if [ $? -ne "0" ]
-then
-    exit -1
-fi
-echo "Install tango-idl"
-docker exec cpp_tango make -C /home/tango/idl/build install
+echo "Run cmake cppTango in $CMAKE_BUILD_TYPE mode"
+echo "Using COVERALLS=$COVERALLS"
 
-echo "Build cppTango:$CMAKE_BUILD_TYPE"
-docker exec cpp_tango cmake -H/home/tango/src -B/home/tango/src/build -DCMAKE_VERBOSE_MAKEFILE=true -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE
+docker exec cpp_tango cmake -H/home/tango/src -B/home/tango/src/build -DCOVERALLS=$COVERALLS -DCOVERALLS_MODULE_PATH=/home/tango/coveralls-cmake/cmake -DCMAKE_VERBOSE_MAKEFILE=true -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE
 if [ $? -ne "0" ]
 then
     exit -1
 fi
 
 docker exec cpp_tango make -C /home/tango/src/build -j 2
-echo "Test log4tango"
-docker exec cpp_tango /bin/sh -c 'cd /home/tango/src/build/log4tango; exec ctest -V'
-echo "Test cppTango"
-docker exec cpp_tango /bin/sh -c 'cd /home/tango/src/build/cpp_test_suite; exec ctest -V'
