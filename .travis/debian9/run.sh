fadf9324dc76eab1a423607c631b97a3a6e46a44
mliszcz <liszcz.michal@gmail.com>
2019-01-30 16:34:22 +0100

    Add support for precompiling server/tango.h

--- .travis/debian9/run.sh
+++ .travis/debian9/run.sh
@@ -2,7 +2,13 @@
 docker exec cpp_tango mkdir -p /home/tango/src/build
 
 echo "Build cppTango:$CMAKE_BUILD_TYPE"
-docker exec cpp_tango cmake -H/home/tango/src -B/home/tango/src/build -DCMAKE_VERBOSE_MAKEFILE=true -DCPPZMQ_BASE=/home/tango -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE
+docker exec cpp_tango cmake \
+  -H/home/tango/src \
+  -B/home/tango/src/build \
+  -DCMAKE_VERBOSE_MAKEFILE=true \
+  -DCPPZMQ_BASE=/home/tango \
+  -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE \
+  -DUSE_PCH=${USE_PCH:-OFF}
 if [ $? -ne "0" ]
 then
     exit -1
