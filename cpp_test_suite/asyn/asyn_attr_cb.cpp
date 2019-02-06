02919144bb2afebad773348cf5e95c80614b6fe2
Thomas Braun <thomas.braun@byte-physics.de>
2019-02-06 09:14:25 +0100

    Asyn Tests/asyn_attr_cb.cpp: Add missing std namespace

--- cpp_test_suite/asyn/asyn_attr_cb.cpp
+++ cpp_test_suite/asyn/asyn_attr_cb.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
  * example of a client using the TANGO device api.
  */
 
@@ -15,9 +15,9 @@ class MyCallBack: public CallBack
 {
 public:
 	MyCallBack():cb_executed(0),to(false),attr_failed(false) {};
-	
+
 	virtual void attr_read(AttrReadEvent *);
-	
+
 	long cb_executed;
 	bool to;
 	bool attr_failed;
@@ -28,12 +28,12 @@ public:
 
 void MyCallBack::attr_read(AttrReadEvent *att)
 {
-	coutv << "In attr_read method for device " << att->device->dev_name() << endl;
+	coutv << "In attr_read method for device " << att->device->dev_name() << std::endl;
 	for (unsigned int i = 0;i < att->attr_names.size();i++)
-		coutv << "Attribute read = " << att->attr_names[i] << endl;
-		
+		coutv << "Attribute read = " << att->attr_names[i] << std::endl;
+
 	nb_attr = att->attr_names.size();
-	
+
 	if (att->err == false)
 	{
 		try
@@ -48,51 +48,51 @@ void MyCallBack::attr_read(AttrReadEvent *att)
 			if (strcmp(e.errors[nb_err - 1].reason,"API_AttributeFailed") == 0)
 			{
 				attr_failed = true;
-				coutv << "Read attributes failed error" << endl;
+				coutv << "Read attributes failed error" << std::endl;
 			}
 			else
-				coutv << "Unknown error" << endl;		
+				coutv << "Unknown error" << std::endl;
 		}
 	}
 	else
 	{
 		long nb_err = att->errors.length();
-		coutv << "read_attributes returns error" << endl;
-		coutv << "error length = " << nb_err << endl;
+		coutv << "read_attributes returns error" << std::endl;
+		coutv << "error length = " << nb_err << std::endl;
 		for (int i = 0;i < nb_err;i++)
-			coutv << "error[" << i << "].reason = " << att->errors[i].reason << endl;
-		
+			coutv << "error[" << i << "].reason = " << att->errors[i].reason << std::endl;
+
 		attr_failed = true;
 		if (strcmp(att->errors[nb_err - 1].reason,"API_DeviceTimedOut") == 0)
 		{
 			to = true;
-			coutv << "Timeout error" << endl;
+			coutv << "Timeout error" << std::endl;
 		}
 	}
-	
+
 	cb_executed++;
 	delete att->argout;
-}	
+}
 
 int main(int argc, char **argv)
 {
 	DeviceProxy *device;
-	
+
 	if (argc == 1)
 	{
-		cout << "usage: %s device [-v]" << endl;
+		cout << "usage: %s device [-v]" << std::endl;
 		exit(-1);
 	}
 
-	string device_name = argv[1];
+	std::string device_name = argv[1];
 
 	if (argc == 3)
 	{
 		if (strcmp(argv[2],"-v") == 0)
 			verbose = true;
 	}
-	
-	try 
+
+	try
 	{
 		device = new DeviceProxy(device_name);
 	}
@@ -102,46 +102,46 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	coutv << endl << "new DeviceProxy(" << device->name() << ") returned" << endl << endl;
+	coutv << std::endl << "new DeviceProxy(" << device->name() << ") returned" << std::endl << std::endl;
 
 	MyCallBack cb;
-	
+
 	try
 	{
-	
+
 // Change timeout (with a useless name as a workaround for ORBacus bug)
 
 //		device->set_timeout_millis(6000);
-				
+
 // Read one attribute
 
-		
+
 		device->read_attribute_asynch("attr_asyn",cb);
-		
+
 // Check if attribute returned
 
 		long nb_not_arrived = 0;
 		while (cb.cb_executed == 0)
 		{
-			coutv << "Attribute not read yet" << endl;
+			coutv << "Attribute not read yet" << std::endl;
 			nb_not_arrived++;
 			Tango_sleep(1);
-			
+
 			device->get_asynch_replies();
 
 		}
-		
+
 		assert( cb.db == 5.55 );
 		assert ( nb_not_arrived >= 2);
-		
-		cout << "   Asynchronous read_attribute in callback mode --> OK" << endl;
+
+		cout << "   Asynchronous read_attribute in callback mode --> OK" << std::endl;
 
 // Read attribute to check polling with blocking with timeout
 
 		cb.cb_executed = 0;
 		cb.db = 0;
 		device->read_attribute_asynch("attr_asyn",cb);
-		
+
 // Check if attribute returned
 
 		nb_not_arrived = 0;
@@ -153,38 +153,38 @@ int main(int argc, char **argv)
 			}
 			catch (AsynReplyNotArrived &e)
 			{
-				coutv << "Attribute not yet read" << endl;
+				coutv << "Attribute not yet read" << std::endl;
 				nb_not_arrived++;
 			}
 		}
-		
+
 		assert ( cb.db == 5.55 );
 		assert ( nb_not_arrived >= 4);
 		assert ( cb.cb_executed == 1);
-		
-		cout << "   Asynchronous read_attribute (callback) in blocking mode with call timeout --> OK" << endl;
-		
+
+		cout << "   Asynchronous read_attribute (callback) in blocking mode with call timeout --> OK" << std::endl;
+
 // Send a command to check polling with blocking
 
 		cb.cb_executed = 0;
 		cb.db = 0;
 		device->read_attribute_asynch("attr_asyn",cb);
-		
+
 // Check if command returned
 
 		device->get_asynch_replies(0);
-		
+
 		assert( cb.db == 5.55 );
 		assert( cb.cb_executed == 1);
 
-		cout << "   Asynchronous read_attribute (callback) in blocking mode --> OK" << endl;
+		cout << "   Asynchronous read_attribute (callback) in blocking mode --> OK" << std::endl;
 
 //---------------------------------------------------------------------------
 //
 //			Now test Timeout exception and asynchronous calls
 //
 //---------------------------------------------------------------------------
-		
+
 // Change timeout in order to test asynchronous calls and timeout
 
 //		device->set_timeout_millis(2000);
@@ -194,31 +194,31 @@ int main(int argc, char **argv)
 		cb.cb_executed = 0;
 		cb.db = 0;
 		device->read_attribute_asynch("attr_asyn_to",cb);
-		
+
 // Check if attribute returned
 
 		nb_not_arrived = 0;
 		while (cb.cb_executed == 0)
 		{
 			nb_not_arrived++;
-			coutv << "Attribute not yet read" << endl;
+			coutv << "Attribute not yet read" << std::endl;
 			Tango_sleep(1);
-			
+
 			device->get_asynch_replies();
 		}
 
 		assert ( cb.to == true );
 		assert ( nb_not_arrived >= 2 );
-		
-		cout << "   Device timeout exception with non blocking get_asynch_replies --> OK" << endl;
-		
+
+		cout << "   Device timeout exception with non blocking get_asynch_replies --> OK" << std::endl;
+
 // Read an attribute to check timeout with polling and blocking with timeout
 
 		cb.cb_executed = 0;
 		cb.db = 0.0;
 		cb.to = false;
 		device->read_attribute_asynch("attr_asyn_to",cb);
-		
+
 // Check if command returned
 
 		nb_not_arrived = 0;
@@ -230,30 +230,30 @@ int main(int argc, char **argv)
 			}
 			catch (AsynReplyNotArrived )
 			{
-				coutv << "Attribute not yet read" << endl;
+				coutv << "Attribute not yet read" << std::endl;
 				nb_not_arrived++;
 			}
 		}
 		assert( cb.to == true );
 		assert( nb_not_arrived >= 2);
-		
-		cout << "   Device timeout with blocking get_asynch_replies with call timeout --> OK" << endl;
-		
+
+		cout << "   Device timeout with blocking get_asynch_replies with call timeout --> OK" << std::endl;
+
 // Read an attribute to check polling with blocking
 
 		cb.cb_executed = 0;
 		cb.db = 0.0;
 		cb.to = false;
-		
+
 		device->read_attribute_asynch("attr_asyn_to",cb);
-		
+
 // Check if attribute returned
 
 		device->get_asynch_replies(0);
 
 		assert(cb.to == true );
-		
-		cout << "   Device timeout with blocking get_asynch_replies --> OK" << endl;
+
+		cout << "   Device timeout with blocking get_asynch_replies --> OK" << std::endl;
 
 //---------------------------------------------------------------------------
 //
@@ -261,7 +261,7 @@ int main(int argc, char **argv)
 //
 //---------------------------------------------------------------------------
 
-		cout << "   Waiting for server to execute all previous requests" << endl;
+		cout << "   Waiting for server to execute all previous requests" << std::endl;
 		Tango_sleep(5);
 
 // Change timeout in order to test asynchronous calls and DevFailed exception
@@ -275,33 +275,33 @@ int main(int argc, char **argv)
 		cb.to = false;
 		cb.attr_failed = false;
 		device->read_attribute_asynch("attr_asyn_except",cb);
-		
+
 // Check if attribute returned
 
 		nb_not_arrived = 0;
 		while (cb.cb_executed == 0)
 		{
 			nb_not_arrived++;
-			coutv << "Attribute not yet read" << endl;
+			coutv << "Attribute not yet read" << std::endl;
 			Tango_sleep(1);
-				
+
 			device->get_asynch_replies();
 		}
 
 		assert ( cb.attr_failed == true );
 		assert ( nb_not_arrived >= 2);
-		
-		cout << "   Device exception with non blocking get_asynch_replies --> OK" << endl;
-		
+
+		cout << "   Device exception with non blocking get_asynch_replies --> OK" << std::endl;
+
 // Read an attribute to check timeout with polling and blocking with timeout
 
 		cb.cb_executed = 0;
 		cb.db = 0.0;
 		cb.attr_failed = false;
 		nb_not_arrived = 0;
-		
+
 		device->read_attribute_asynch("attr_asyn_except",cb);
-		
+
 // Check if attribute returned
 
 		while (cb.cb_executed == 0)
@@ -312,7 +312,7 @@ int main(int argc, char **argv)
 			}
 			catch (AsynReplyNotArrived )
 			{
-				coutv << "Attribute not yet read" << endl;
+				coutv << "Attribute not yet read" << std::endl;
 				nb_not_arrived++;
 			}
 
@@ -320,24 +320,24 @@ int main(int argc, char **argv)
 		assert( cb.attr_failed == true );
 		assert(cb.cb_executed == 1);
 		assert( nb_not_arrived >= 2);
-		
-		cout << "   Device exception with blocking get_asynch_replies with call timeout --> OK" << endl;
-		
+
+		cout << "   Device exception with blocking get_asynch_replies with call timeout --> OK" << std::endl;
+
 // Read an attribute to check polling with blocking
 
 		cb.cb_executed = 0;
 		cb.attr_failed = false;
-		
+
 		device->read_attribute_asynch("attr_asyn_except",cb);
-		
+
 // Check if attribute returned
 
 		device->get_asynch_replies(0);
 
 		assert(cb.attr_failed == true );
 		assert(cb.cb_executed == 1);
-				
-		cout << "   Device exception with blocking get_asynch_replies --> OK" << endl;
+
+		cout << "   Device exception with blocking get_asynch_replies --> OK" << std::endl;
 
 //---------------------------------------------------------------------------
 //
@@ -350,33 +350,33 @@ int main(int argc, char **argv)
 		cb.nb_attr = 0;
 		cb.db = 0.0;
 		cb.cb_executed = 0;
-		
-		vector<string> na;
+
+		std::vector<std::string> na;
 		na.push_back("attr_asyn");
 		na.push_back("Short_attr");
-				
+
 		device->read_attributes_asynch(na,cb);
-		
+
 // Check if attribute returned
 
 		nb_not_arrived = 0;
 		while (cb.cb_executed == 0)
 		{
-			coutv << "Attributes not read yet" << endl;
+			coutv << "Attributes not read yet" << std::endl;
 			nb_not_arrived++;
 			Tango_sleep(1);
-			
+
 			device->get_asynch_replies();
 
 		}
-		
+
 		assert( cb.db == 5.55 );
 		assert ( nb_not_arrived >= 2);
 		assert ( cb.nb_attr == 2);
 		assert (cb.sh == 12);
-		
-		cout << "   Several attributes in read_attributes in callback mode --> OK" << endl;
-								
+
+		cout << "   Several attributes in read_attributes in callback mode --> OK" << std::endl;
+
 	}
 	catch (Tango::DevFailed &e)
 	{
@@ -390,6 +390,6 @@ int main(int argc, char **argv)
 	}
 
 	delete device;
-	
+
 	return 0;
 }
