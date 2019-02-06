09adbb6f6afa7903e5b7ddb2023c6058eaf4a930
Thomas Braun <thomas.braun@byte-physics.de>
2019-02-06 09:29:33 +0100

    Test DS/FwdTestClass.cpp: Add missing std namespace

--- cpp_test_suite/cpp_test_ds/fwd_ds/FwdTestClass.cpp
+++ cpp_test_suite/cpp_test_ds/fwd_ds/FwdTestClass.cpp
@@ -13,23 +13,23 @@ static const char *HttpServer = "http://www.esrf.eu/computing/cs/tango/tango_doc
 //               and all properties and methods required
 //               by the FwdTest once per process.
 //
-// project :     
+// project :
 //
 // This file is part of Tango device class.
-// 
+//
 // Tango is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // Tango is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with Tango.  If not, see <http://www.gnu.org/licenses/>.
-// 
+//
 // $Author$
 //
 // $Revision$
@@ -74,23 +74,23 @@ FwdTestClass *FwdTestClass::_instance = NULL;
 
 //--------------------------------------------------------
 /**
- * method : 		FwdTestClass::FwdTestClass(string &s)
+ * method : 		FwdTestClass::FwdTestClass(std::string &s)
  * description : 	constructor for the FwdTestClass
  *
  * @param s	The class name
  */
 //--------------------------------------------------------
-FwdTestClass::FwdTestClass(string &s):Tango::DeviceClass(s)
+FwdTestClass::FwdTestClass(std::string &s):Tango::DeviceClass(s)
 {
-	cout2 << "Entering FwdTestClass constructor" << endl;
+	cout2 << "Entering FwdTestClass constructor" << std::endl;
 	set_default_property();
 	write_class_property();
 
 	/*----- PROTECTED REGION ID(FwdTestClass::constructor) ENABLED START -----*/
-	
+
 	/*----- PROTECTED REGION END -----*/	//	FwdTestClass::constructor
 
-	cout2 << "Leaving FwdTestClass constructor" << endl;
+	cout2 << "Leaving FwdTestClass constructor" << std::endl;
 }
 
 //--------------------------------------------------------
@@ -102,7 +102,7 @@ FwdTestClass::FwdTestClass(string &s):Tango::DeviceClass(s)
 FwdTestClass::~FwdTestClass()
 {
 	/*----- PROTECTED REGION ID(FwdTestClass::destructor) ENABLED START -----*/
-	
+
 	/*----- PROTECTED REGION END -----*/	//	FwdTestClass::destructor
 
 	_instance = NULL;
@@ -124,14 +124,14 @@ FwdTestClass *FwdTestClass::init(const char *name)
 	{
 		try
 		{
-			string s(name);
+			std::string s(name);
 			_instance = new FwdTestClass(s);
 		}
-		catch (bad_alloc &)
+		catch (std::bad_alloc &)
 		{
 			throw;
-		}		
-	}		
+		}
+	}
 	return _instance;
 }
 
@@ -146,7 +146,7 @@ FwdTestClass *FwdTestClass::instance()
 {
 	if (_instance == NULL)
 	{
-		cerr << "Class is not initialised !!" << endl;
+		std::cerr << "Class is not initialised !!" << std::endl;
 		exit(-1);
 	}
 	return _instance;
@@ -167,7 +167,7 @@ FwdTestClass *FwdTestClass::instance()
  *	Description : Get the class property for specified name.
  */
 //--------------------------------------------------------
-Tango::DbDatum FwdTestClass::get_class_property(string &prop_name)
+Tango::DbDatum FwdTestClass::get_class_property(std::string &prop_name)
 {
 	for (unsigned int i=0 ; i<cl_prop.size() ; i++)
 		if (cl_prop[i].name == prop_name)
@@ -182,7 +182,7 @@ Tango::DbDatum FwdTestClass::get_class_property(string &prop_name)
  *	Description : Return the default value for device property.
  */
 //--------------------------------------------------------
-Tango::DbDatum FwdTestClass::get_default_device_property(string &prop_name)
+Tango::DbDatum FwdTestClass::get_default_device_property(std::string &prop_name)
 {
 	for (unsigned int i=0 ; i<dev_def_prop.size() ; i++)
 		if (dev_def_prop[i].name == prop_name)
@@ -197,7 +197,7 @@ Tango::DbDatum FwdTestClass::get_default_device_property(string &prop_name)
  *	Description : Return the default value for class property.
  */
 //--------------------------------------------------------
-Tango::DbDatum FwdTestClass::get_default_class_property(string &prop_name)
+Tango::DbDatum FwdTestClass::get_default_class_property(std::string &prop_name)
 {
 	for (unsigned int i=0 ; i<cl_def_prop.size() ; i++)
 		if (cl_def_prop[i].name == prop_name)
@@ -218,10 +218,10 @@ Tango::DbDatum FwdTestClass::get_default_class_property(string &prop_name)
 //--------------------------------------------------------
 void FwdTestClass::set_default_property()
 {
-	string	prop_name;
-	string	prop_desc;
-	string	prop_def;
-	vector<string>	vect_data;
+	std::string	prop_name;
+	std::string	prop_desc;
+	std::string	prop_def;
+	std::vector<std::string>	vect_data;
 
 	//	Set Default Class Properties
 
@@ -241,42 +241,42 @@ void FwdTestClass::write_class_property()
 		return;
 
 	Tango::DbData	data;
-	string	classname = get_name();
-	string	header;
-	string::size_type	start, end;
+	std::string	classname = get_name();
+	std::string	header;
+	std::string::size_type	start, end;
 
 	//	Put title
 	Tango::DbDatum	title("ProjectTitle");
-	string	str_title("");
+	std::string	str_title("");
 	title << str_title;
 	data.push_back(title);
 
 	//	Put Description
 	Tango::DbDatum	description("Description");
-	vector<string>	str_desc;
+	std::vector<std::string>	str_desc;
 	str_desc.push_back("");
 	description << str_desc;
 	data.push_back(description);
 
 	//	put cvs or svn location
-	string	filename("FwdTest");
+	std::string	filename("FwdTest");
 	filename += "Class.cpp";
 
 	// check for cvs information
-	string	src_path(CvsPath);
+	std::string	src_path(CvsPath);
 	start = src_path.find("/");
-	if (start!=string::npos)
+	if (start!= std::string::npos)
 	{
 		end   = src_path.find(filename);
 		if (end>start)
 		{
-			string	strloc = src_path.substr(start, end-start);
+			std::string	strloc = src_path.substr(start, end-start);
 			//	Check if specific repository
 			start = strloc.find("/cvsroot/");
-			if (start!=string::npos && start>0)
+			if (start!= std::string::npos && start>0)
 			{
-				string	repository = strloc.substr(0, start);
-				if (repository.find("/segfs/")!=string::npos)
+				std::string	repository = strloc.substr(0, start);
+				if (repository.find("/segfs/")!= std::string::npos)
 					strloc = "ESRF:" + strloc.substr(start, strloc.length()-start);
 			}
 			Tango::DbDatum	cvs_loc("cvs_location");
@@ -288,17 +288,17 @@ void FwdTestClass::write_class_property()
 	// check for svn information
 	else
 	{
-		string	src_path(SvnPath);
+		std::string	src_path(SvnPath);
 		start = src_path.find("://");
-		if (start!=string::npos)
+		if (start!= std::string::npos)
 		{
 			end = src_path.find(filename);
 			if (end>start)
 			{
 				header = "$HeadURL: ";
 				start = header.length();
-				string	strloc = src_path.substr(start, (end-start));
-				
+				std::string	strloc = src_path.substr(start, (end-start));
+
 				Tango::DbDatum	svn_loc("svn_location");
 				svn_loc << strloc;
 				data.push_back(svn_loc);
@@ -307,40 +307,40 @@ void FwdTestClass::write_class_property()
 	}
 
 	//	Get CVS or SVN revision tag
-	
+
 	// CVS tag
-	string	tagname(TagName);
+	std::string	tagname(TagName);
 	header = "$Name: ";
 	start = header.length();
-	string	endstr(" $");
-	
+	std::string	endstr(" $");
+
 	end   = tagname.find(endstr);
-	if (end!=string::npos && end>start)
+	if (end!= std::string::npos && end>start)
 	{
-		string	strtag = tagname.substr(start, end-start);
+		std::string	strtag = tagname.substr(start, end-start);
 		Tango::DbDatum	cvs_tag("cvs_tag");
 		cvs_tag << strtag;
 		data.push_back(cvs_tag);
 	}
-	
+
 	// SVN tag
-	string	svnpath(SvnPath);
+	std::string	svnpath(SvnPath);
 	header = "$HeadURL: ";
 	start = header.length();
-	
+
 	end   = svnpath.find(endstr);
-	if (end!=string::npos && end>start)
+	if (end!= std::string::npos && end>start)
 	{
-		string	strloc = svnpath.substr(start, end-start);
-		
-		string tagstr ("/tags/");
+		std::string	strloc = svnpath.substr(start, end-start);
+
+		std::string tagstr ("/tags/");
 		start = strloc.find(tagstr);
-		if ( start!=string::npos )
+		if ( start!= std::string::npos )
 		{
 			start = start + tagstr.length();
 			end   = strloc.find(filename);
-			string	strtag = strloc.substr(start, end-start-1);
-			
+			std::string	strtag = strloc.substr(start, end-start-1);
+
 			Tango::DbDatum	svn_tag("svn_tag");
 			svn_tag << strtag;
 			data.push_back(svn_tag);
@@ -348,7 +348,7 @@ void FwdTestClass::write_class_property()
 	}
 
 	//	Get URL location
-	string	httpServ(HttpServer);
+	std::string	httpServ(HttpServer);
 	if (httpServ.length()>0)
 	{
 		Tango::DbDatum	db_doc_url("doc_url");
@@ -358,7 +358,7 @@ void FwdTestClass::write_class_property()
 
 	//  Put inheritance
 	Tango::DbDatum	inher_datum("InheritedFrom");
-	vector<string> inheritance;
+	std::vector<std::string> inheritance;
 	inheritance.push_back("Tango::Device_4Impl");
 	inher_datum << inheritance;
 	data.push_back(inher_datum);
@@ -381,16 +381,16 @@ void FwdTestClass::write_class_property()
 void FwdTestClass::device_factory(const Tango::DevVarStringArray *devlist_ptr)
 {
 	/*----- PROTECTED REGION ID(FwdTestClass::device_factory_before) ENABLED START -----*/
-	
+
 	//	Add your own code
-	
+
 	/*----- PROTECTED REGION END -----*/	//	FwdTestClass::device_factory_before
 
 	//	Create devices and add it into the device list
 	for (unsigned long i=0 ; i<devlist_ptr->length() ; i++)
 	{
-		cout4 << "Device name : " << (*devlist_ptr)[i].in() << endl;
-		device_list.push_back(new FwdTest(this, (*devlist_ptr)[i]));							 
+		cout4 << "Device name : " << (*devlist_ptr)[i].in() << std::endl;
+		device_list.push_back(new FwdTest(this, (*devlist_ptr)[i]));
 	}
 
 	//	Manage dynamic attributes if any
@@ -411,9 +411,9 @@ void FwdTestClass::device_factory(const Tango::DevVarStringArray *devlist_ptr)
 	}
 
 	/*----- PROTECTED REGION ID(FwdTestClass::device_factory_after) ENABLED START -----*/
-	
+
 	//	Add your own code
-	
+
 	/*----- PROTECTED REGION END -----*/	//	FwdTestClass::device_factory_after
 }
 //--------------------------------------------------------
@@ -423,10 +423,10 @@ void FwdTestClass::device_factory(const Tango::DevVarStringArray *devlist_ptr)
  *                and store them in the attribute list
  */
 //--------------------------------------------------------
-void FwdTestClass::attribute_factory(vector<Tango::Attr *> &att_list)
+void FwdTestClass::attribute_factory(std::vector<Tango::Attr *> &att_list)
 {
 	/*----- PROTECTED REGION ID(FwdTestClass::attribute_factory_before) ENABLED START -----*/
-	
+
 	FwdAttrScaSh	*att1 = new FwdAttrScaSh("fwd_short_rw");
 	Tango::UserDefaultFwdAttrProp	att1_prop;
 	att1_prop.set_label("Gasp a fwd attribute");
@@ -441,14 +441,14 @@ void FwdTestClass::attribute_factory(vector<Tango::Attr *> &att_list)
 
 	FwdAttrImaStr	*att4 = new FwdAttrImaStr("fwd_ima_string_rw");
 	att_list.push_back(att4);
-	
+
 	/*----- PROTECTED REGION END -----*/	//	FwdTestClass::attribute_factory_before
 	//	Create a list of static attributes
 	create_static_attribute_list(get_class_attr()->get_attr_list());
 	/*----- PROTECTED REGION ID(FwdTestClass::attribute_factory_after) ENABLED START -----*/
-	
+
 	//	Add your own code
-	
+
 	/*----- PROTECTED REGION END -----*/	//	FwdTestClass::attribute_factory_after
 }
 //--------------------------------------------------------
@@ -461,16 +461,16 @@ void FwdTestClass::attribute_factory(vector<Tango::Attr *> &att_list)
 void FwdTestClass::command_factory()
 {
 	/*----- PROTECTED REGION ID(FwdTestClass::command_factory_before) ENABLED START -----*/
-	
+
 	//	Add your own code
-	
+
 	/*----- PROTECTED REGION END -----*/	//	FwdTestClass::command_factory_before
 
 
 	/*----- PROTECTED REGION ID(FwdTestClass::command_factory_after) ENABLED START -----*/
-	
+
 	//	Add your own code
-	
+
 	/*----- PROTECTED REGION END -----*/	//	FwdTestClass::command_factory_after
 }
 
@@ -483,22 +483,22 @@ void FwdTestClass::command_factory()
  * method : 		FwdTestClass::create_static_attribute_list
  * description : 	Create the a list of static attributes
  *
- * @param	att_list	the ceated attribute list 
+ * @param	att_list	the ceated attribute list
  */
 //--------------------------------------------------------
-void FwdTestClass::create_static_attribute_list(vector<Tango::Attr *> &att_list)
+void FwdTestClass::create_static_attribute_list(std::vector<Tango::Attr *> &att_list)
 {
 	for (unsigned long i=0 ; i<att_list.size() ; i++)
 	{
-		string att_name(att_list[i]->get_name());
-		transform(att_name.begin(), att_name.end(), att_name.begin(), ::tolower);
+		std::string att_name(att_list[i]->get_name());
+		std::transform(att_name.begin(), att_name.end(), att_name.begin(), ::tolower);
 		defaultAttList.push_back(att_name);
 	}
 
-	cout2 << defaultAttList.size() << " attributes in default list" << endl;
+	cout2 << defaultAttList.size() << " attributes in default list" << std::endl;
 
 	/*----- PROTECTED REGION ID(FwdTestClass::create_static_att_list) ENABLED START -----*/
-	
+
 	/*----- PROTECTED REGION END -----*/	//	FwdTestClass::create_static_att_list
 }
 
@@ -512,26 +512,26 @@ void FwdTestClass::create_static_attribute_list(vector<Tango::Attr *> &att_list)
  * @param	list of all attributes
  */
 //--------------------------------------------------------
-void FwdTestClass::erase_dynamic_attributes(const Tango::DevVarStringArray *devlist_ptr, vector<Tango::Attr *> &att_list)
+void FwdTestClass::erase_dynamic_attributes(const Tango::DevVarStringArray *devlist_ptr, std::vector<Tango::Attr *> &att_list)
 {
 	Tango::Util *tg = Tango::Util::instance();
 
 	for (unsigned long i=0 ; i<devlist_ptr->length() ; i++)
-	{	
-		Tango::DeviceImpl *dev_impl = tg->get_device_by_name(((string)(*devlist_ptr)[i]).c_str());
+	{
+		Tango::DeviceImpl *dev_impl = tg->get_device_by_name(((std::string)(*devlist_ptr)[i]).c_str());
 		FwdTest *dev = static_cast<FwdTest *> (dev_impl);
-		
-		vector<Tango::Attribute *> &dev_att_list = dev->get_device_attr()->get_attribute_list();
-		vector<Tango::Attribute *>::iterator ite_att;
+
+		std::vector<Tango::Attribute *> &dev_att_list = dev->get_device_attr()->get_attribute_list();
+		std::vector<Tango::Attribute *>::iterator ite_att;
 		for (ite_att=dev_att_list.begin() ; ite_att != dev_att_list.end() ; ++ite_att)
 		{
-			string att_name((*ite_att)->get_name_lower());
+			std::string att_name((*ite_att)->get_name_lower());
 			if ((att_name == "state") || (att_name == "status"))
 				continue;
-			vector<string>::iterator ite_str = find(defaultAttList.begin(), defaultAttList.end(), att_name);
+			std::vector<std::string>::iterator ite_str = find(defaultAttList.begin(), defaultAttList.end(), att_name);
 			if (ite_str == defaultAttList.end())
 			{
-				cout2 << att_name << " is a UNWANTED dynamic attribute for device " << (*devlist_ptr)[i] << endl;
+				cout2 << att_name << " is a UNWANTED dynamic attribute for device " << (*devlist_ptr)[i] << std::endl;
 				Tango::Attribute &att = dev->get_device_attr()->get_attr_by_name(att_name.c_str());
 				dev->remove_attribute(att_list[att.get_attr_idx()], true, false);
 				--ite_att;
@@ -539,7 +539,7 @@ void FwdTestClass::erase_dynamic_attributes(const Tango::DevVarStringArray *devl
 		}
 	}
 	/*----- PROTECTED REGION ID(FwdTestClass::erase_dynamic_attributes) ENABLED START -----*/
-	
+
 	/*----- PROTECTED REGION END -----*/	//	FwdTestClass::erase_dynamic_attributes
 }
 
@@ -549,9 +549,9 @@ void FwdTestClass::erase_dynamic_attributes(const Tango::DevVarStringArray *devl
  *	Description : returns Tango::Attr * object found by name
  */
 //--------------------------------------------------------
-Tango::Attr *FwdTestClass::get_attr_object_by_name(vector<Tango::Attr *> &att_list, string attname)
+Tango::Attr *FwdTestClass::get_attr_object_by_name(std::vector<Tango::Attr *> &att_list, std::string attname)
 {
-	vector<Tango::Attr *>::iterator it;
+	std::vector<Tango::Attr *>::iterator it;
 	for (it=att_list.begin() ; it<att_list.end() ; it++)
 		if ((*it)->get_name()==attname)
 			return (*it);
