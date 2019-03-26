2cc4a91dc4de98361da0927316216462b0512181
Reynald Bourtembourg <bourtemb@users.noreply.github.com>
2019-03-26 16:40:28 +0100

    Workaround issues with Debian7-8 docker containers (#549)
    * Revert "Remove Debian 7 Travis builds... (#547)"
    This reverts commit eaec2a0319b86b7dce8cc4c5f61d5e51f2a46a35.
    * Update Debian 7 Travis Docker file (#546) ...
    Use archive.debian.org to get wheezy packages since wheezy has reached end of
    life and its packages have been moved to archive.debian.org
    Remove duplicated curl installation
    * Update Travis Debian 8 Docker file
    Work-around while waiting for upstream debian:jessie docker file to be updated
    after jessie-updates have been removed from Debian mirrors
    Remove duplicated curl installation

---
+++ .travis/debian7/run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+docker exec cpp_tango mkdir -p /home/tango/src/build
+
+echo "Build cppTango:$CMAKE_BUILD_TYPE"
+docker exec cpp_tango cmake -H/home/tango/src -B/home/tango/src/build -DCMAKE_VERBOSE_MAKEFILE=true -DCPPZMQ_BASE=/home/tango -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE
+if [ $? -ne "0" ]
+then
+    exit -1
+fi
+
+docker exec cpp_tango make -C /home/tango/src/build -j 2
