bcd7951d42de0e0da6f247b5c9f805d1d7d8a853
mliszcz <liszcz.michal@gmail.com>
2019-05-31 23:18:50 +0200

    Docker setup improvements for test suite
    * Remove -p, so that another instance can use port 10000
    
    * Speedup DB setup by disabling InnoDB
    
    * Use tango_admin to wait until the DB goes online
    
    * Use tango_admin to wait until device servers are online

--- cpp_test_suite/environment/pre_test.sh.cmake
+++ cpp_test_suite/environment/pre_test.sh.cmake
@@ -9,11 +9,14 @@ echo "Run conf_devtest"
 echo "Start DevTest"
 echo "DevTest libraries:"
 echo "`ldd @PROJECT_BINARY_DIR@/cpp_test_ds/DevTest`"
-#TODO Starter?
 @PROJECT_BINARY_DIR@/cpp_test_ds/DevTest @INST_NAME@ -v5 1>@PROJECT_BINARY_DIR@/cpp_test_ds/DevTest_@INST_NAME@.out 2>&1 &
 echo $! > @PROJECT_BINARY_DIR@/cpp_test_ds/DevTest_@INST_NAME@.pid
 
-sleep 3
+if hash tango_admin 2>/dev/null; then
+    tango_admin --ping-device dserver/devtest/@INST_NAME@ 3
+else
+    sleep 3
+fi
 
 echo "Start FwdTest"
 echo "FwdTest libraries:"
@@ -21,4 +24,8 @@ echo "`ldd @PROJECT_BINARY_DIR@/cpp_test_ds/fwd_ds/FwdTest`"
 @PROJECT_BINARY_DIR@/cpp_test_ds/fwd_ds/FwdTest @INST_NAME@ -v5 1>@PROJECT_BINARY_DIR@/cpp_test_ds/fwd_ds/FwdTest_@INST_NAME@.out 2>&1 &
 echo $! > @PROJECT_BINARY_DIR@/cpp_test_ds/fwd_ds/FwdTest_@INST_NAME@.pid
 
-sleep 3
+if hash tango_admin 2>/dev/null; then
+    tango_admin --ping-device dserver/fwdtest/@INST_NAME@ 3
+else
+    sleep 3
+fi
--- cpp_test_suite/environment/setup.sh
+++ cpp_test_suite/environment/setup.sh
@@ -5,11 +5,15 @@ echo "Setup test environment"
 #run mysql-tango docker
 docker run --name mysql_db \
     -e MYSQL_ROOT_PASSWORD=root \
-    -d tangocs/mysql:9.2.2 --sql-mode=""
+    -e MYSQL_INITDB_SKIP_TZINFO=1 \
+    -d \
+    tangocs/mysql:9.2.2 \
+    --sql-mode="" \
+    --innodb=OFF \
+    --default-storage-engine=MyISAM
 
 #run tango-cs docker
 CONTAINER=$(docker run --name tango_cs \
-    -p 10000:10000 \
     -e TANGO_HOST=127.0.0.1:10000 \
     -e MYSQL_HOST=mysql_db:3306 \
     -e MYSQL_USER=tango \
@@ -21,17 +25,16 @@ CONTAINER=$(docker run --name tango_cs \
 echo "CONTAINER=$CONTAINER"
 
 IPADDR=$(docker inspect -f '{{ .NetworkSettings.IPAddress }}' $CONTAINER)
-#export TANGO_HOST
-TANGO_HOST=$IPADDR:10000
+export TANGO_HOST=$IPADDR:10000
 echo "TANGO_HOST=$TANGO_HOST"
-export TANGO_HOST
 
 echo "Create tango_host file"
-echo "#!/bin/bash" > tango_host
-echo "export TANGO_HOST=$TANGO_HOST" >> tango_host
+echo "export TANGO_HOST=$TANGO_HOST" > tango_host
 
 echo "Wait till tango-cs is online"
-#TODO notification?
-sleep 30
 
-#ctest
+if hash tango_admin 2>/dev/null; then
+  tango_admin --ping-database 30
+else
+  sleep 30
+fi
