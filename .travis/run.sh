86174eea96f54b7b8d0962029997154a0913e5e8
Thomas Braun <thomas.braun@byte-physics.de>
2019-06-05 19:01:00 +0200

    Travis Tests: Avoid code duplication
    We currently have run scripts for each supported debian version. These
    run scripts are mostly very similiar.
    
    So it makes sense to use a common script.
    
    The differences are if we use COVERALLS or PCH support which we can
    support via environment variables.
    
    We also reduce the number of jobs in preparation for adding debian 10
    (buster) support. We only keep the debug jobs of debian 9 and lower.

---
+++ .travis/run.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "############################"
+echo "CMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE"
+echo "OS_TYPE=$OS_TYPE"
+echo "TANGO_HOST=$TANGO_HOST"
+echo "COVERALLS=$COVERALLS"
+echo "USE_PCH=$USE_PCH"
+echo "TANGO_USE_USING_NAMESPACE=$TANGO_USE_USING_NAMESPACE"
+echo "############################"
+
+docker exec cpp_tango mkdir -p /home/tango/src/build
+
+# set defaults
+MAKEFLAGS=${MAKEFLAGS:- -j 2}
+COVERALLS=${COVERALLS:-OFF}
+USE_PCH=${USE_PCH:-OFF}
+COVERALLS_MODULE_PATH=/home/tango/coveralls-cmake/cmake
+
+docker exec cpp_tango cmake                                \
+  -H/home/tango/src                                        \
+  -B/home/tango/src/build                                  \
+  -DCMAKE_VERBOSE_MAKEFILE=ON                              \
+  -DCPPZMQ_BASE=/home/tango                                \
+  -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}                   \
+  -DUSE_PCH=${USE_PCH}                                     \
+  -DTANGO_USE_USING_NAMESPACE=${TANGO_USE_USING_NAMESPACE} \
+  -DCOVERALLS=${COVERALLS}                                 \
+  -DCOVERALLS_MODULE_PATH=${COVERALLS_MODULE_PATH}
+
+if [[ "$COVERALLS" == "ON" ]]
+then
+  docker exec cpp_tango                                            \
+    /home/tango/build-wrapper-linux-x86/build-wrapper-linux-x86-64 \
+    --out-dir /home/tango/src/bw-output                            \
+    make -C /home/tango/src/build
+else
+  docker exec cpp_tango make -C /home/tango/src/build
+fi
