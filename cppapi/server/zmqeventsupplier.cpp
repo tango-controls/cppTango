e2f5b69a004dc99bb2b98a39f36838a05315ddb0
Thomas Braun <thomas.braun@byte-physics.de>
2019-06-05 18:11:16 +0200

    zmqeventconsumer/zmqeventsupplier: Make it compatible with all stock cppzmq versions
    Depending on the cppzmq version from https://github.com/zeromq/cppzmq the
    operator void* is either implicit or explicit.
    
    We could now add a feature check in cmake to check for that and use code
    paths for each variant.
    
    But we could also be really pragmatic and just use a unconditional
    static_cast<void*>.
    
    Idea and initial patch by Michal Liszcz.

--- cppapi/server/zmqeventsupplier.cpp
+++ cppapi/server/zmqeventsupplier.cpp
@@ -686,7 +686,7 @@ void ZmqEventSupplier::create_mcast_socket(string &mcast_data,int rate,McastSock
 // Bind the publisher socket to the specified port
 //
 
-    if (zmq_bind(*(ms.pub_socket),ms.endpoint.c_str()) != 0)
+    if (zmq_bind(static_cast<void*>(*ms.pub_socket),ms.endpoint.c_str()) != 0)
     {
         TangoSys_OMemStream o;
         o << "Can't bind ZMQ socket with endpoint ";
