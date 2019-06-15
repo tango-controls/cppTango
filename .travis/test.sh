8b708fab95c9dbf556bf6a6ed9e25e3cc95c6b64
mliszcz <liszcz.michal@gmail.com>
2019-06-15 23:39:09 +0200

    Enable new test runner script in CI
    The test runner will start a fresh environment for each test case.

--- .travis/test.sh
+++ .travis/test.sh
@@ -6,22 +6,9 @@ then
     TEST_COMMAND="exec make coveralls"
 fi
 
-echo "PreTest"
-docker exec cpp_tango /bin/sh -c 'cd /home/tango/src/build/cpp_test_suite/environment; exec ./pre_test.sh'
-if [ $? -ne "0" ]
-then
-    exit -1
-fi
 echo "Test"
 echo "TEST_COMMAND=$TEST_COMMAND"
 docker exec cpp_tango /bin/sh -c "cd /home/tango/src/build; $TEST_COMMAND"
-if [ $? -ne "0" ]
-then
-    exit -1
-fi
-
-echo "PostTest"
-docker exec cpp_tango /bin/sh -c 'cd /home/tango/src/build/cpp_test_suite/environment; exec ./post_test.sh'
 if [ $? -ne "0" ]
 then
     exit -1
--- cpp_test_suite/CTestCustom.cmake
+++
@@ -1,2 +0,0 @@
-set(CTEST_CUSTOM_PRE_TEST ${PROJECT_BINARY_DIR}/environment/pre_test.sh)
-set(CTEST_CUSTOM_POST_TEST ${PROJECT_BINARY_DIR}/environment/post_test.sh)
