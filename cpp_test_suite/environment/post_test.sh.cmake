809fb3ab9680ffb2ea942cb2229363bf56999f00
mliszcz <liszcz.michal@gmail.com>
2019-06-16 16:54:59 +0200

    Remove old scripts for running tests

--- cpp_test_suite/environment/post_test.sh.cmake
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#TODO Starter?
-
-PID=$(<@PROJECT_BINARY_DIR@/cpp_test_ds/DevTest_@INST_NAME@.pid)
-kill $PID
-
-PID=$(<@PROJECT_BINARY_DIR@/cpp_test_ds/fwd_ds/FwdTest_@INST_NAME@.pid)
-kill $PID
