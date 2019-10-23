0bfe76418bffe43e9f3bdc1c37b6a84de25a4a8c
Thomas Braun <thomas.braun@byte-physics.de>
2019-10-23 22:24:04 +0200

    CMakeLists.txt: Add option to error out on warnings

--- .travis/run.sh
+++ .travis/run.sh
@@ -9,6 +9,7 @@ echo "TANGO_HOST=$TANGO_HOST"
 echo "COVERALLS=$COVERALLS"
 echo "USE_PCH=$USE_PCH"
 echo "TANGO_USE_USING_NAMESPACE=$TANGO_USE_USING_NAMESPACE"
+echo "WARNINGS_AS_ERRORS=$WARNINGS_AS_ERRORS"
 echo "############################"
 
 docker exec cpp_tango mkdir -p /home/tango/src/build
@@ -17,6 +18,7 @@ docker exec cpp_tango mkdir -p /home/tango/src/build
 MAKEFLAGS=${MAKEFLAGS:- -j 2}
 COVERALLS=${COVERALLS:-OFF}
 USE_PCH=${USE_PCH:-OFF}
+WARNINGS_AS_ERRORS=${WARNINGS_AS_ERRORS:-OFF}
 COVERALLS_MODULE_PATH=/home/tango/coveralls-cmake/cmake
 
 docker exec cpp_tango cmake                                \
@@ -27,6 +29,7 @@ docker exec cpp_tango cmake                                \
   -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}                   \
   -DUSE_PCH=${USE_PCH}                                     \
   -DTANGO_USE_USING_NAMESPACE=${TANGO_USE_USING_NAMESPACE} \
+  -DWARNINGS_AS_ERRORS=${WARNINGS_AS_ERRORS}               \
   -DCOVERALLS=${COVERALLS}                                 \
   -DCOVERALLS_MODULE_PATH=${COVERALLS_MODULE_PATH}
 
