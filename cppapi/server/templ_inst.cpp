bafb413400b5a39885f62d067dce0912d7228d35
Thomas Braun <thomas.braun@byte-physics.de>
2019-10-24 21:24:53 +0200

    server/templ_inst.cpp: Remove explicit instantiation
    Clang 9.0.0 complains with the following warning
    
    /home/firma/devel/cppTango/cppapi/server/templ_inst.cpp:994:24: warning: explicit instantiation of 'operator<<<char>' that occurs after an explicit specialization has no effect [-Winstantiation-after-specialization]
    template std::ostream &operator<<(std::ostream &,DataElement<DevString> &);
                           ^
    /home/firma/devel/cppTango/cppapi/client/devapi_pipe.tpp:166:15: note: previous template specialization is here
    std::ostream &operator<<(std::ostream &str,DataElement<DevString> &dd)
                  ^
    
    Removing the explicit instantation is the right thing to do here.

--- cppapi/server/templ_inst.cpp
+++ cppapi/server/templ_inst.cpp
@@ -990,10 +990,6 @@ template std::ostream &operator<<(std::ostream &,DataElement<DevState> &);
 //template std::ostream &operator<<(std::ostream &,DataElement<DevEncoded> &);
 template std::ostream &operator<<(std::ostream &,DataElement<const std::string> &);
 
-#ifndef _TG_WINDOWS_
-template std::ostream &operator<<(std::ostream &,DataElement<DevString> &);
-#endif
-
 template std::ostream &operator<<(std::ostream &,DataElement<std::vector<DevBoolean> > &);
 template std::ostream &operator<<(std::ostream &,DataElement<std::vector<short> > &);
 template std::ostream &operator<<(std::ostream &,DataElement<std::vector<DevLong> > &);
