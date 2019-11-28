a112e0a4a639dd3683ce629c9265e5a63b65d5ca
mliszcz <liszcz.michal@gmail.com>
2019-11-28 15:22:48 +0100

    Use tango_admin to check if servers are running
    Use tango_admin (if available) in tests and CI jobs to check
    if device servers are up and running.

--- cpp_test_suite/cxxtest/bin/start_server.sh.cmake
+++ cpp_test_suite/cxxtest/bin/start_server.sh.cmake
@@ -5,7 +5,12 @@ start_server(){
     @PROJECT_BINARY_DIR@/cpp_test_ds/DevTest $1 -v5 1>@PROJECT_BINARY_DIR@/cpp_test_ds/DevTest_$1.out 2>&1 &
     echo $! > @PROJECT_BINARY_DIR@/cpp_test_ds/DevTest_$1.pid
 
-    sleep 7
+    if hash tango_admin 2>/dev/null; then
+        tango_admin --ping-device "dserver/devtest/$1" 7
+    else
+        sleep 7
+    fi
+
     echo "Done. PID="`cat @PROJECT_BINARY_DIR@/cpp_test_ds/DevTest_$1.pid`
 }
 
--- cpp_test_suite/environment/pre_test.sh.cmake
+++ cpp_test_suite/environment/pre_test.sh.cmake
@@ -13,7 +13,11 @@ echo "`ldd @PROJECT_BINARY_DIR@/cpp_test_ds/DevTest`"
 @PROJECT_BINARY_DIR@/cpp_test_ds/DevTest @INST_NAME@ -v5 1>@PROJECT_BINARY_DIR@/cpp_test_ds/DevTest_@INST_NAME@.out 2>&1 &
 echo $! > @PROJECT_BINARY_DIR@/cpp_test_ds/DevTest_@INST_NAME@.pid
 
-sleep 3
+if hash tango_admin 2>/dev/null; then
+    tango_admin --ping-device "dserver/devtest/@INST_NAME@" 3
+else
+    sleep 3
+fi
 
 echo "Start FwdTest"
 echo "FwdTest libraries:"
@@ -21,4 +25,8 @@ echo "`ldd @PROJECT_BINARY_DIR@/cpp_test_ds/fwd_ds/FwdTest`"
 @PROJECT_BINARY_DIR@/cpp_test_ds/fwd_ds/FwdTest @INST_NAME@ -v5 1>@PROJECT_BINARY_DIR@/cpp_test_ds/fwd_ds/FwdTest_@INST_NAME@.out 2>&1 &
 echo $! > @PROJECT_BINARY_DIR@/cpp_test_ds/fwd_ds/FwdTest_@INST_NAME@.pid
 
-sleep 3
+if hash tango_admin 2>/dev/null; then
+    tango_admin --ping-device "dserver/fwdtest/@INST_NAME@" 3
+else
+    sleep 3
+fi
