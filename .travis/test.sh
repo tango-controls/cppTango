e6d96a7b472888e63b466ab3d1552d1669ad675a
Thomas Braun <thomas.braun@byte-physics.de>
2020-01-23 23:59:06 +0100

    .travis/test.sh: Move set -e declaration to the top

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [ $RUN_TESTS = "OFF" ]
 then
   echo "Skipping tests as requested"
@@ -15,8 +17,6 @@ function run_in_container {
     -e CTEST_OUTPUT_ON_FAILURE=ON \
     cpp_tango "$@"
 }
-
-set -e
 
 if [[ "$COVERALLS" == "ON" ]]; then
   run_in_container make coveralls
