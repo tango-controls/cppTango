3a0f66124c61de531aa146f846b0178cb9b0251a
Thomas Braun <thomas.braun@byte-physics.de>
2020-01-28 12:21:27 +0100

    changes

--- .travis/sonar.sh
+++ .travis/sonar.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     SONAR_PROPERTIES=""
 else
     SONAR_PROPERTIES="-Dsonar.pullrequest.base=$TRAVIS_BRANCH -Dsonar.pullrequest.branch=$TRAVIS_PULL_REQUEST_BRANCH -Dsonar.pullrequest.key=$TRAVIS_PULL_REQUEST"
 fi
+
 echo "Using SONAR_PROPERTIES=$SONAR_PROPERTIES"
+
 git fetch --unshallow --quiet
-cd /home/tango/src
+
 export PATH=$PATH:$(ls -d $HOME/sonar-scanner*/bin)
+echo $PATH
+ls -l $HOME
+
+cd /home/tango/src
 sonar-scanner $SONAR_PROPERTIES
