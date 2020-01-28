f9aff6254005d84ca3af19d64b64cd7e8362c4fd
Thomas Braun <thomas.braun@byte-physics.de>
2020-01-28 20:50:33 +0100

    changes

--- .travis/sonar.sh
+++ .travis/sonar.sh
@@ -5,7 +5,7 @@ set -e
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     SONAR_PROPERTIES=""
 else
-    SONAR_PROPERTIES="-Dsonar.pullrequest.base=$TRAVIS_BRANCH -Dsonar.pullrequest.branch=$TRAVIS_PULL_REQUEST_BRANCH -Dsonar.pullrequest.key=$TRAVIS_PULL_REQUEST -Dsonar.login=t-b@github -Dsonar.password=${SONAR_TOKEN}"
+    SONAR_PROPERTIES="-Dsonar.pullrequest.base=$TRAVIS_BRANCH -Dsonar.pullrequest.branch=$TRAVIS_PULL_REQUEST_BRANCH -Dsonar.pullrequest.key=$TRAVIS_PULL_REQUEST -Dsonar.login=${SONAR_TOKEN}"
 fi
 
 git fetch --unshallow --quiet
