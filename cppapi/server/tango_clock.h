59d2c5f59ad0bf8468dfb7188d5e4270ef6da4ee
mliszcz <liszcz.michal@gmail.com>
2020-07-10 14:32:11 +0200

    Rename namespace __detail to detail

--- cppapi/server/tango_clock.h
+++ cppapi/server/tango_clock.h
@@ -16,7 +16,7 @@ static_assert(
     std::ratio_less_equal<PollClock::duration::period, std::micro>::value,
     "Polling thread requires microsecond precision");
 
-namespace __detail
+namespace detail
 {
 
 template <typename A, typename B>
@@ -51,7 +51,7 @@ std::chrono::system_clock::time_point make_system_time(TS sec, TU usec, TN nsec)
     return system_clock::time_point{timestamp_sys};
 }
 
-} // namespace __detail
+} // namespace detail
 
 template <typename Clock, typename Dur>
 TimeVal make_TimeVal(std::chrono::time_point<Clock, Dur> tp)
@@ -59,7 +59,7 @@ TimeVal make_TimeVal(std::chrono::time_point<Clock, Dur> tp)
     constexpr std::chrono::nanoseconds::rep NANOS_IN_SEC = 1000*1000*1000;
     constexpr std::chrono::nanoseconds::rep NANOS_IN_USEC = 1000;
 
-    auto wall_tp = __detail::convert_time_point<std::chrono::system_clock::time_point>(tp);
+    auto wall_tp = detail::convert_time_point<std::chrono::system_clock::time_point>(tp);
     auto time_ns = std::chrono::nanoseconds(wall_tp.time_since_epoch()).count();
     TimeVal tv{};
     tv.tv_sec = time_ns / NANOS_IN_SEC;
@@ -80,19 +80,19 @@ template <typename Clock, typename Dur>
 
 inline PollClock::time_point make_poll_time(::timeval tv)
 {
-    auto sys_time = __detail::make_system_time(tv.tv_sec, tv.tv_usec, 0);
-    return __detail::convert_time_point<PollClock::time_point>(sys_time);
+    auto sys_time = detail::make_system_time(tv.tv_sec, tv.tv_usec, 0);
+    return detail::convert_time_point<PollClock::time_point>(sys_time);
 }
 
 inline PollClock::time_point make_poll_time(TimeVal tv)
 {
-    auto sys_time = __detail::make_system_time(tv.tv_sec, tv.tv_usec, tv.tv_nsec);
-    return __detail::convert_time_point<PollClock::time_point>(sys_time);
+    auto sys_time = detail::make_system_time(tv.tv_sec, tv.tv_usec, tv.tv_nsec);
+    return detail::convert_time_point<PollClock::time_point>(sys_time);
 }
 
 inline std::chrono::system_clock::time_point make_system_time(::timeval tv)
 {
-    return __detail::make_system_time(tv.tv_sec, tv.tv_usec, 0);
+    return detail::make_system_time(tv.tv_sec, tv.tv_usec, 0);
 }
 
 } // namespace Tango
