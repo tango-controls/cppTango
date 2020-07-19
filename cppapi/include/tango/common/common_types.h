e5cc42046c9affa83b2e485a014e7a8d2899e2a4
mliszcz <liszcz.michal@gmail.com>
2020-07-19 18:23:20 +0200

    Add common_types header

---
+++ cppapi/include/tango/common/common_types.h
@@ -0,0 +1,12 @@
+#ifndef TANGO_PUBLIC_COMMON_TYPES_H
+#define TANGO_PUBLIC_COMMON_TYPES_H
+
+namespace Tango
+{
+
+using ConstDevString = const char*;
+
+} // namespace Tango
+
+#endif // TANGO_PUBLIC_COMMON_TYPES_H
+
