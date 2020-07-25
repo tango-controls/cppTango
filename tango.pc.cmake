fa83349d64ee18e5cd3df3dc1b179679262987a9
mliszcz <liszcz.michal@gmail.com>
2020-07-25 12:13:55 +0200

    Add ${prefix}/include to search path in tango.pc
    Public headers are referenced using full paths, so a parent directory
    like /usr/local/include is needed in the compiler search path.

--- tango.pc.cmake
+++ tango.pc.cmake
@@ -1,11 +1,11 @@
 prefix = @CMAKE_INSTALL_PREFIX@
 exec_prefix = ${prefix}
-includedir = ${prefix}/include/tango
+includedir = ${prefix}/include
 libdir = ${prefix}/@CMAKE_INSTALL_LIBDIR@
 
 Name: @CMAKE_PROJECT_NAME@
 Description: Tango client/server API library
 Version: @LIBRARY_VERSION@
-Cflags: -I${includedir}
+Cflags: -I${includedir}/tango -I${includedir}
 Requires: libzmq omniORB4 omniCOS4 omniDynamic4
 Libs: -L${libdir} -ltango -lzmq -lomniORB4 -lomnithread -lCOS4 -lomniDynamic4
