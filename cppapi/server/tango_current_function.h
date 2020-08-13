de8891966f7d65a392f373fffb8cfbc4834dac7f
mliszcz <liszcz.michal@gmail.com>
2020-08-13 16:33:07 +0200

    Rename BOOST_CURRENT_FUNCTION to TANGO_CURRENT_FUNCTION
    This is needed to avoid conflicts during compilation of device servers
    where boost/current_function.hpp is also included.

--- cppapi/server/tango_current_function.h
+++ cppapi/server/tango_current_function.h
@@ -4,24 +4,22 @@
 // Adapted from: https://www.boost.org/doc/libs/1_73_0/boost/current_function.hpp
 //
 #if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__) || defined(__clang__)
-# define BOOST_CURRENT_FUNCTION __PRETTY_FUNCTION__
+# define TANGO_CURRENT_FUNCTION __PRETTY_FUNCTION__
 #elif defined(__DMC__) && (__DMC__ >= 0x810)
-# define BOOST_CURRENT_FUNCTION __PRETTY_FUNCTION__
+# define TANGO_CURRENT_FUNCTION __PRETTY_FUNCTION__
 #elif defined(__FUNCSIG__)
-# define BOOST_CURRENT_FUNCTION __FUNCSIG__
+# define TANGO_CURRENT_FUNCTION __FUNCSIG__
 #elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
-# define BOOST_CURRENT_FUNCTION __FUNCTION__
+# define TANGO_CURRENT_FUNCTION __FUNCTION__
 #elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
-# define BOOST_CURRENT_FUNCTION __FUNC__
+# define TANGO_CURRENT_FUNCTION __FUNC__
 #elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
-# define BOOST_CURRENT_FUNCTION __func__
+# define TANGO_CURRENT_FUNCTION __func__
 #elif defined(__cplusplus) && (__cplusplus >= 201103)
-# define BOOST_CURRENT_FUNCTION __func__
+# define TANGO_CURRENT_FUNCTION __func__
 #else
-# define BOOST_CURRENT_FUNCTION "(unknown)"
+# define TANGO_CURRENT_FUNCTION "(unknown)"
 #endif
-
-#define TANGO_CURRENT_FUNCTION BOOST_CURRENT_FUNCTION
 
 #define TANGO_QUOTE_(s) #s
 #define TANGO_QUOTE(s) TANGO_QUOTE_(s)
