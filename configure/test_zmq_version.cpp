ccd76476f863001eb533cb34a063f759d7541052
Thomas Braun <thomas.braun@byte-physics.de>
2020-10-21 13:27:03 +0200

    configure/CMakeLists.txt: Enforce a minimum libzmq version
    The autotools build system of the tango source distribution already
    enforces a minimum libzmq version of 4.0.5.
    
    We now do the same for the CMake build system.
    
    This is a pure compile time check, so it works with cross compiling. And
    as it does not use pkg-config, it can also later be ported to other
    non-linux platforms like Windows.
    
    Close #788.

---
+++ configure/test_zmq_version.cpp
@@ -0,0 +1,15 @@
+#include <zmq.h>
+
+// Check libzmq version using plain C++03
+
+#define MINIMUM_VERSION ZMQ_MAKE_VERSION(MINIMUM_VERSION_MAJOR, MINIMUM_VERSION_MINOR, MINIMUM_VERSION_PATCH)
+#define FOUND_VERSION ZMQ_MAKE_VERSION(ZMQ_VERSION_MAJOR, ZMQ_VERSION_MINOR, ZMQ_VERSION_PATCH)
+
+#if FOUND_VERSION < MINIMUM_VERSION
+#error "Old version"
+#endif
+
+int main(int, char**)
+{
+	return 0;
+}
